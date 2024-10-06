abc46f3b4c74295247a5dc5903e7746ebd4e18c7
MuhammadAtteq <muhammadatteq7866@gmail.com>
2024-10-06 13:07:44 +0500

    initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/Ameer-Hamza289/portfolio'
+      className='link footer__link' target="_blank"
+    >
+    ©2023 All rights reserved - Muhammad Atteq
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/components/Home/Home.js
@@ -0,0 +1,46 @@
+import "./Home.css"
+import Typewriter from "typewriter-effect";
+import { Email, Phone } from '@material-ui/icons'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
+
+function Home()
+{
+
+    return(
+
+        <section className="section home center">
+         <h1>
+          Hi, I am <span className='about__name'>Muhammad Atteq</span>
+        </h1>
+    
+      <h2 className="typer"> 
+        <Typewriter options={{strings:["A Full Stack Web Developer","Curious to learn new things"],
+        autoStart:true,loop:true,deleteSpeed:50}}/>
+      </h2> 
+
+
+        <p id="des">Adaptable and responsible web developer capable of writing production ready code using HTML, CSS, JavaScript,
+         React.Js on the front-end and NodeJs, Express.Js, and MongoDB on the back-end to build single-page applications.</p>
+  
+ 
+       <section id="social">
+         <a href="https://github.com/atteq786" target="_blank" aria-label='github' className='link link--icon'> <GitHubIcon /> </a>
+         
+         <a href="https://www.linkedin.com/in/muhammad-atteq" target="_blank" aria-label='linkedin' className='link link--icon'> <LinkedInIcon /> </a>
+         
+         <a href='mailto:atteq7866@gmail.com' aria-label="email" className='link link--icon'> <Email/> </a>
+
+         <a href='tel:+923144247022' aria-label="phone" className='link link--icon'> <Phone/> </a>
+    
+      </section>
+     
+   
+         <a href="https://drive.google.com/file/d/17zQpegNc6vJZecQTOcTCJ6697ycNigXM/view?usp=sharing" target="_blank" id="resumeButton">
+            <span type='button' className='btn btn--outline'>  Resume </span>
+          </a>
+   
+    </section>)
+}
+
+export default Home
