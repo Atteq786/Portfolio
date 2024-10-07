a307d8ab50e5d2ffee7c9b75d0aea553fcc07b17
MuhammadAtteq <muhammadatteq7866@gmail.com>
2024-10-07 21:58:30 +0500

    main

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => (
       href='https://github.com/Ameer-Hamza289/portfolio'
       className='link footer__link' target="_blank"
     >
-    ©2023 All rights reserved - Muhammad Atteq
+    ©2024 All rights reserved - Muhammad Atteq
     </a>
   </footer>
 )
